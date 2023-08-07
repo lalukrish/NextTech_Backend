f53e554aa3210b8832a3e55abcbe832b6d1bd674
junsila <junsila77@gmail.c0m>
2023-08-07 23:33:40 +0530

    conflict fixed

--- Routers/adminRouter.js
+++ Routers/adminRouter.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const Admin_Router = require("../Controllers/adminUserController");
 
+
+
+const router = express.Router();
+
 router.post("/block-user", Admin_Router.blockUser);
 router.post("/unblock-user", Admin_Router.unblockUser);
 
--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+const adminRouter=require("./Routers/adminRouter")
 const app = express();
 const userRouter = require("./Routers/userRouter");
 
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", userRouter);
-//app.use("/admin");
+app.use("/admin",adminRouter);
 const DevServer = () => {
   app.listen(6000, () => {
     console.log(`server running on port${6000}`);
@@ -30,4 +30,3 @@ const DevServer = () => {
 };
 
 DevServer();
-console.log("hi");
