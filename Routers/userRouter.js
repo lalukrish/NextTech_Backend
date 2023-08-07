224f5f34bf7de2eb1eb04c8abb36ba596fe3952f
lalukrish <lalukrishnavallil01@gmail.com>
2023-08-07 11:38:14 +0530

    try

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const Login_Signup_Controller = require("../Controllers/loginSingupController");
 
-const router = express.Router();
+const userRouter = express.Router();
 
-router.post("/user-signup", Login_Signup_Controller.userSignup);
-router.post("/user-signin", Login_Signup_Controller.userSignin);
-router.get("/get-all-users", Login_Signup_Controller.getAllUser);
-router.get("/get-one-user/:userId", Login_Signup_Controller.getSingleUser);
+userRouter.post("/user-signup", Login_Signup_Controller.userSignup);
+userRouter.post("/user-signin", Login_Signup_Controller.userSignin);
+userRouter.get("/get-all-users", Login_Signup_Controller.getAllUser);
+userRouter.get("/get-one-user/:userId", Login_Signup_Controller.getSingleUser);
 
-module.exports = router;
+module.exports = userRouter;
