191daf92ac8d0b977465d8394f074a33b82ffb88
theetha1608 <theerthakunjusss@gmail.com>
2023-08-24 20:16:06 +0530

    settings

--- Routers/courseRouter.js
+++ Routers/courseRouter.js
@@ -6,7 +6,6 @@ const Login_Signup_Controller = require("../Controllers/courseController");
 const router = express.Router();
 
 router.post("/user-signup", Login_Signup_Controller.userSignup);
-
-
+router.get("/create-course", Course_Controller.createCourse);
 
 module.exports = router;
--- Schema/courseSchema.js
+++ Schema/courseSchema.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const courseSchema = new mongoose.Schema({
+  course_id: {
+    type: String,
+    required: true,
+  },
   course_name: {
     type: String,
     required: true,
