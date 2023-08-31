6cb9683c66de9ae62997ad2c8188489c6acbb2ce
lalukrish <lalukrishnavallil01@gmail.com>
2023-08-31 20:44:06 +0530

    posts:get all and single posts

--- Schema/userSchema.js
+++ Schema/userSchema.js
@@ -1,55 +1,54 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  full_name: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    full_name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    user_name: {
+      type: String,
+      required: true,
+    },
+    phone_number: {
+      type: String,
+      required: true,
+    },
+    country_code: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    admin: {
+      type: Boolean,
+      default: false,
+    },
+    block: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    profile_image_url: {
+      type: String,
+    },
+    profile_image_public_id: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  user_name: {
-    type: String,
-    required: true,
-  },
-  phone_number: {
-    type: String,
-    required: true,
-  },
-  country_code: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  admin: {
-    type: Boolean,
-    default: false,
-  },
-  block: {
-    type: Boolean,
-    default: false,
-  },
-  role: {
-    type: String,
-    default: "user",
-  },
-  profile_image_url: {
-    type: String,
-  },
-  profile_image_public_id: {
-    type: String,
-  },
-  // posts: [
-  //   {
-  //     type: mongoose.Schema.Types.ObjectId,
-  //     ref: "Posts", // Reference the Posts model
-  //   },
-  // ],
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Users = mongoose.model("Users", userSchema);
 
