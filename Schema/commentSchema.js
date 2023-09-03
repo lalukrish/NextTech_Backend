b31a17d5325965e622712d331f6a032ec5df4881
lalukrish <lalukrishnavallil01@gmail.com>
2023-09-03 19:29:44 +0530

    feat:time stamp added on the comment

--- Schema/commentSchema.js
+++ Schema/commentSchema.js
@@ -1,42 +1,51 @@
 const mongoose = require("mongoose");
 
-const commentSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: true,
-  },
-  likes: {
-    type: Number,
-    default: 0,
-  },
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Users", // Reference to a User model
-    required: true,
-  },
-  post: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Posts", // Reference to a Post model
-    required: true,
-  },
-  replies: [
-    {
-      reply_text: {
-        type: String,
-        required: true,
-      },
-      reply_likes: {
-        type: Number,
-        default: 0,
-      },
-      author: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Users", // Reference to a User model
-        required: true,
-      },
+const commentSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
     },
-  ],
-});
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users", // Reference to a User model
+      required: true,
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Posts", // Reference to a Post model
+      required: true,
+    },
+
+    replies: [
+      {
+        reply_text: {
+          type: String,
+          required: true,
+        },
+        reply_likes: {
+          type: Number,
+          default: 0,
+        },
+        author: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Users", // Reference to a User model
+          required: true,
+        },
+      },
+      {
+        timestamps: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Comments = mongoose.model("Comments", commentSchema);
 module.exports = Comments;
